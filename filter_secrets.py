d1b3eca7b7197ce6b9038e767834ac7206b2b711
lordminx <admin@story-games.at>
2017-02-06 11:23:15 +0100

    Now get tags to filter from script name/symlink

--- filter_secrets.py
+++ filter_secrets.py
@@ -1,11 +1,21 @@
 #! /usr/bin/env python
 
+import os
+
 from panflute import *
+
 
 def filter_secret(elem, doc):
     if isinstance(elem, Div):
-        if "secret" in elem.classes:
+        if get_tag() in elem.classes:
             return []
+
+
+def get_tag():
+    filename = os.path.basename(__file__)
+    tag = filename.split("_")[1].split(".")[0]
+    return tag
+
 
 
 
